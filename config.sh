aceccc3aaccfbff05848410486095fe9a315638f
redallen <zallen@redhat.com>
2020-10-23 12:33:28 -0400

    remove unused boilerplate

---
+++ config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+StackName=SlackInviter
+
+S3BucketArtifacts=patternfly-lambda
+S3PrefixArtifacts=cloudformation/slack-invite-automation
+
+CommunityName="PatternFly"
+SlackUrl=patternfly.slack.com
+SlackToken=xoxp-77120091684-542908134870-1473283313456-47037ff0da90fa0cffc74137edfd6a7e
+InviteToken=
+RecaptchaSiteKey=
+RecaptchaSecretKey=
+Locale=en
+
